1a26fb432b540bdf0df05c3f5fbaf14be4069ae2
lngrss <magdalenagrossmann@gmx.de>
2020-05-16 21:55:04 +0200

    Finishing tree class

--- search_algorith/tree.rb
+++ search_algorith/tree.rb
@@ -9,13 +9,13 @@ class Tree
 
   def build_tree(arr)
     if arr.size < 2
-      if arr
+      unless arr.empty?
         root = Node.new(arr[0])
       end
     else
       root = Node.new(arr[arr.size / 2])
       root.left_node = build_tree(arr[0...(arr.size / 2)])
-      root.right_node = build_tree(arr[(arr.size / 2)..])
+      root.right_node = build_tree(arr[((arr.size / 2) + 1)..])
     end
     root
   end
@@ -88,7 +88,7 @@ class Tree
           end
           node = nil
 
-        else ## and now shit is happening
+        else
           ## find max from left
           parent = node
           potential = node.left_node
@@ -127,36 +127,121 @@ class Tree
   end
 
   def level_order
-#     ＼＼ ＿
-# 　　 ＼(　•_•) F
-# 　　　 <　⌒ヽ A
-# 　　　/ 　 へ＼ B
-# 　　 /　　/　＼＼ U
-# 　　 |　ノ　　 ヽ_つ L
-# 　　/　/ O
-# 　 /　/| U
-# 　(　(ヽ S
-# 　|　|、＼
-# 　| 丿 ＼ ⌒)
-# 　| |　　) /
-# `ノ )　　Lﾉ
+    if block_given?
+      queue = [@root]
+      queue.each do |item|
+        if item
+          yield item
+          queue << item.left_node if item.left_node
+          queue << item.right_node if item.right_node
+        end
+        queue.drop(1)
+      end
+    else
+      queue = [@root]
+      result = []
+      queue.each do |item|
+        if item
+          result << item.value
+          queue << item.left_node if item.left_node
+          queue << item.right_node if item.right_node
+        end
+        queue.drop(1)
+      end
+      result
+    end
   end
 
-  def inorder
+  def inorder(node=@root, &block)
+    if block_given?
+      if !node.left_node && !node.right_node ##leaf element
+        yield node
+      else
+        inorder(node.left_node, &block) if node.left_node
+        yield node
+        inorder(node.right_node, &block) if node.right_node
+      end
+    else
+      result = []
+      if !node.left_node && !node.right_node
+        result << node.value
+      else
+        result += inorder(node.left_node) if node.left_node
+        result << node.value
+        result += inorder(node.right_node) if node.right_node
+      end
+      result
+    end
   end
 
-  def preorder
+  def preorder(node=@root, &block)
+    if block_given?
+      if !node.left_node && !node.right_node ##leaf element
+        yield node
+      else
+        yield node
+        preorder(node.left_node, &block) if node.left_node
+        preorder(node.right_node, &block) if node.right_node
+      end
+    else
+      result = []
+      if !node.left_node && !node.right_node
+        result << node.value
+      else
+        result << node.value
+        result += preorder(node.left_node) if node.left_node
+        result += preorder(node.right_node) if node.right_node
+      end
+      result
+    end
   end
 
-  def postorder
+  def postorder(node=@root, &block)
+    if block_given?
+      if !node.left_node && !node.right_node ##leaf element
+        yield node
+      else
+        postorder(node.left_node, &block) if node.left_node
+        postorder(node.right_node, &block) if node.right_node
+        yield node
+      end
+    else
+      result = []
+      if !node.left_node && !node.right_node
+        result << node.value
+      else
+        result += postorder(node.left_node) if node.left_node
+        result += postorder(node.right_node) if node.right_node
+        result << node.value
+      end
+      result
+    end
   end
 
   def depth(node)
+    count = -1
+    searched = []
+    to_be_searched = [node]
+    while to_be_searched.any? do
+      searched = to_be_searched
+      to_be_searched = []
+      searched.each do |node|
+        to_be_searched << node.left_node if node.left_node
+        to_be_searched << node.right_node if node.right_node
+      end
+      count += 1
+    end
+    count
   end
 
-  def balanced?
+  def balanced?(node=@root)
+    left_balanced = node.left_node ? balanced?(node.left_node) : true
+    right_balanced = node.right_node ? balanced?(node.right_node) : true
+    height_difference_ok = (depth(node.left_node) - depth(node.right_node)).abs < 2
+    left_balanced && right_balanced && height_difference_ok
   end
 
   def rebalance!
+    @root = build_tree(inorder)
   end
 end
